ae132e5c6357ab4344eda08518ab0534e5454b98
dacoquima <danimalistica@gmail.com>
2019-07-20 16:01:24 -0400

    Se hace responsive en CSS y se ordena la data por letra y numero en data.js

--- src/data.js
+++ src/data.js
@@ -1,10 +1,30 @@
 const dataResult = window.RICKANDMORTY.results;
 
-let filter = (dataResult, select) => {
-  let filtro = dataResult.filter(element => {
-    return element.status === select;
+let filter = (dataResult, selectFilter) => {
+  let filters = dataResult.filter(element => {
+    return element.status === selectFilter;
   })
 
-return filtro; 
+return filters; 
 }
 window.filter = filter;
+
+const order = (dataResult, selectOrder) => {
+  let filterOrder = '';
+    if (selectOrder === "1-493") {
+      filterOrder = dataResult.sort((a, b) => {
+          return a.id - b.id
+      })} else if (selectOrder === "493-1") {
+          filterOrder = dataResult.sort((a, b) => {
+          return b.id - a.id
+      })} else if (selectOrder === "A-Z") {
+          filterOrder = dataResult.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+      })} else if (selectOrder === "Z-A") {
+          filterOrder = dataResult.sort((a, b) => {
+          return b.name.localeCompare(a.name);
+      })
+  }
+return filterOrder; 
+ }
+window.order = order;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const data = window.RICKANDMORTY.results;
 
 for (let i = 0; i < data.length; i++) {
-
+ 
 //CARD
 card = document.createElement("div");
 card.className = "flip-card";
@@ -67,13 +67,12 @@ document.getElementById("container").appendChild(card).innerHTML;
 
 let filterHTML = document.getElementById('filterHTML');
 filterHTML.addEventListener('change', () => {
-let select = filterHTML.options[filterHTML.selectedIndex].value; 
-let result = window.filter(dataResult, select);
+let selectFilter = filterHTML.options[filterHTML.selectedIndex].value; 
+let resultFilter = window.filter(dataResult, selectFilter);
 document.getElementById("container").innerHTML=''; 
 
-for (let i = 0; i < result.length; i++) {
+for (let i = 0; i < resultFilter.length; i++) {
 
-//CARD
 card = document.createElement("div");
 card.className = "flip-card";
 
@@ -85,38 +84,106 @@ cardFront.className = "flip-card-front";
 
 let cardBack = document.createElement("div");
 cardBack.className = "flip-card-back";
- 
-//CONTENT 
+  
 let character = document.createElement("h4");
-character.textContent = result[i].name;
+character.textContent = resultFilter[i].name;
 character.className = "characters";
 
 let image = document.createElement("img");
-image.src = result[i].image;
+image.src = resultFilter[i].image;
 image.className = "images";
  
 let image2 = document.createElement("img");
-image2.src = result[i].image;
+image2.src = resultFilter[i].image;
 image2.className = "images2";
 
 let number = document.createElement("p");
-number.textContent =  "#" + result[i].id;
+number.textContent =  "#" + resultFilter[i].id;
 number.className = "numbers";
 
 let status = document.createElement("h3");
-status.textContent = "Status: " + result[i].status;
+status.textContent = "Status: " + resultFilter[i].status;
 status.className = "status";
 
 let locationName = document.createElement("h3");
-locationName.textContent = "Location: " + result[i].location.name;
+locationName.textContent = "Location: " + resultFilter[i].location.name;
 locationName.className = "locations";
 
 let specie = document.createElement("h3");
-specie.textContent = "Specie: " + result[i].species;
+specie.textContent = "Specie: " + resultFilter[i].species;
 specie.className = "species";
 
 let origin = document.createElement("h3");
-origin.textContent = "Origin: " + result[i].origin.name;
+origin.textContent = "Origin: " + resultFilter[i].origin.name;
+origin.className = "origins";
+
+card.appendChild(cardInner);
+cardInner.appendChild(cardFront);
+cardFront.appendChild(image);
+cardFront.appendChild(number);
+cardFront.appendChild(character);
+cardInner.appendChild(cardBack);
+cardBack.appendChild(image2);
+cardBack.appendChild(status);
+cardBack.appendChild(specie);
+cardBack.appendChild(origin);
+
+document.getElementById("container").appendChild(card).innerHTML; 
+}});
+
+
+// Order
+
+let orderHTML = document.getElementById('orderHTML');
+orderHTML.addEventListener('change', () => {
+let selectOrder = orderHTML.options[orderHTML.selectedIndex].value; 
+let resultOrder = window.order(dataResult, selectOrder);
+document.getElementById("container").innerHTML=''; 
+
+for (let i = 0; i < resultOrder.length; i++) {
+
+card = document.createElement("div");
+card.className = "flip-card";
+
+let cardInner = document.createElement("div");
+cardInner.className = "flip-card-inner";
+
+let cardFront = document.createElement("div");
+cardFront.className = "flip-card-front";
+
+let cardBack = document.createElement("div");
+cardBack.className = "flip-card-back";
+  
+let character = document.createElement("h4");
+character.textContent = resultOrder[i].name;
+character.className = "characters";
+
+let image = document.createElement("img");
+image.src = resultOrder[i].image;
+image.className = "images";
+ 
+let image2 = document.createElement("img");
+image2.src = resultOrder[i].image;
+image2.className = "images2";
+
+let number = document.createElement("p");
+number.textContent =  "#" + resultOrder[i].id;
+number.className = "numbers";
+
+let status = document.createElement("h3");
+status.textContent = "Status: " + resultOrder[i].status;
+status.className = "status";
+
+let locationName = document.createElement("h3");
+locationName.textContent = "Location: " + resultOrder[i].location.name;
+locationName.className = "locations";
+
+let specie = document.createElement("h3");
+specie.textContent = "Specie: " + resultOrder[i].species;
+specie.className = "species";
+
+let origin = document.createElement("h3");
+origin.textContent = "Origin: " + resultOrder[i].origin.name;
 origin.className = "origins";
 
 card.appendChild(cardInner);
